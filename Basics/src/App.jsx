3cdd64ee69a8dcf317921cc4cfd87d6db8b24cc5
ankitrajput9 <rajputankit9630@gmail.com>
2025-11-01 08:44:39 +0530

    useState hook

---
+++ Basics/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Usestate from './components/useState/Usestate';
+
+const App = () => {
+  return (
+    <div >
+  <Usestate/>
+    </div>
+  );
+}
+
+export default App;
