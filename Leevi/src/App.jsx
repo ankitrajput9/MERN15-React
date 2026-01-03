ab83aa5c1d9e2c0f405b48637a853c5f4a9a97d2
ankitrajput9 <rajputankit9630@gmail.com>
2026-01-03 13:00:53 +0530

    Leevi Website

---
+++ Leevi/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='px-4'>
+  <div className='flex flex-col gap-4'>
+    <h1 className='text-5xl text-center'>Create digital 
+innovations.</h1>
+<p className='text-center'>Elevate your digital presence with stunning design and development.</p>
+  </div>
+  <div>
+    <img src="" alt="" />
+  </div>
+    </div>
+  );
+}
+
+export default App;
